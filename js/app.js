1b79a62db23c4702060d4dbd18ea8ed9e56dc5b8
AAlukard <Presario1>
2015-04-03 09:27:38 +0300

    Added map

---
+++ js/app.js
@@ -0,0 +1,438 @@
+/**
+ * Created by alukard on 4/3/15.
+ */
+
+$(document).ready(function() {
+
+    $(".pepsi-button").on('click', function() {
+        renderMap(pepsi);
+    });
+    $(".cola-button").on('click', function() {
+        renderMap(cola);
+    });
+
+    var election = new Datamap({
+        scope: 'usa',
+        element: document.getElementById('container'),
+        geographyConfig: {
+            highlightBorderColor: '#bada55',
+            highlightBorderWidth: 3
+        },
+        fills: {
+            'bad': '#8cacff',
+            'somewhat-bad': '#8d6bff',
+            'neutral': '#b187ff',
+            'somewhat-good': '#ff87fb',
+            'good': '#ff8e8e',
+             defaultFill: 'rgba(23,48,210,0.9)'
+        },
+        data: {}
+    });
+    election.labels();
+});
+
+var cola = {
+    "AZ": {
+        "fillKey": "bad",
+        "electoralVotes": 5
+    },
+    "CO": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 5
+    },
+    "DE": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "FL": {
+        "fillKey": "bad",
+        "electoralVotes": 29
+    },
+    "GA": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "HI": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "ID": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "IL": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "IN": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 11
+    },
+    "IA": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 11
+    },
+    "KS": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "KY": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "LA": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "MD": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "ME": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "MA": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "MN": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "MI": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "MS": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "MO": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 13
+    },
+    "MT": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "NC": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "NE": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "NV": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "NH": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "NJ": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "NY": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "ND": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "NM": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "OH": {
+        "fillKey": "good",
+        "electoralVotes": 32
+    },
+    "OK": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "OR": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "PA": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "RI": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "SC": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "SD": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "TN": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "TX": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "UT": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "WI": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "VA": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 32
+    },
+    "VT": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "WA": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "WV": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "WY": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "CA": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "CT": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "AK": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "AR": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "AL": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    }
+};
+
+var pepsi = {
+    "AZ": {
+        "fillKey": "bad",
+        "electoralVotes": 5
+    },
+    "CO": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 5
+    },
+    "DE": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "FL": {
+        "fillKey": "neutral",
+        "electoralVotes": 29
+    },
+    "GA": {
+        "fillKey": "good",
+        "electoralVotes": 32
+    },
+    "HI": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "ID": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "IL": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "IN": {
+        "fillKey": "good",
+        "electoralVotes": 11
+    },
+    "IA": {
+        "fillKey": "bad",
+        "electoralVotes": 11
+    },
+    "KS": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "KY": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "LA": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "MD": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "ME": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "MA": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 32
+    },
+    "MN": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 32
+    },
+    "MI": {
+        "fillKey": "good",
+        "electoralVotes": 32
+    },
+    "MS": {
+        "fillKey": "good",
+        "electoralVotes": 32
+    },
+    "MO": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 13
+    },
+    "MT": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 32
+    },
+    "NC": {
+        "fillKey": "good",
+        "electoralVotes": 32
+    },
+    "NE": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 32
+    },
+    "NV": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "NH": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "NJ": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "NY": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "ND": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "NM": {
+        "fillKey": "good",
+        "electoralVotes": 32
+    },
+    "OH": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "OK": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "OR": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "PA": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "RI": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "SC": {
+        "fillKey": "good",
+        "electoralVotes": 32
+    },
+    "SD": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 32
+    },
+    "TN": {
+        "fillKey": "good",
+        "electoralVotes": 32
+    },
+    "TX": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "UT": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "WI": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "VA": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "VT": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "WA": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "WV": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "WY": {
+        "fillKey": "somewhat-bad",
+        "electoralVotes": 32
+    },
+    "CA": {
+        "fillKey": "bad",
+        "electoralVotes": 32
+    },
+    "CT": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    },
+    "AK": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "AR": {
+        "fillKey": "neutral",
+        "electoralVotes": 32
+    },
+    "AL": {
+        "fillKey": "somewhat-good",
+        "electoralVotes": 32
+    }
+}
